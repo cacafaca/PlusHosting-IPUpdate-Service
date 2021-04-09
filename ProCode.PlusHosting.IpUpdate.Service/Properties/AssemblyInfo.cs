62c8d464709b83e357c919458dc18864fd9805bb
Nemanja Simović <nemanja.simovic@gmail.com>
2021-04-09 02:21:56 +0200

    Changed file version of service just in case.

--- ProCode.PlusHosting.IpUpdate.Service/Properties/AssemblyInfo.cs
+++ ProCode.PlusHosting.IpUpdate.Service/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
 
 // Below directive can force access to internal objects. I need to access LoginInfo class,
 // so I can have class only in one place. 
